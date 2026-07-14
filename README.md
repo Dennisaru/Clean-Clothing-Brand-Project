cf11ee5d6c717c86120f0f394d6a25289c545739
Dennisaru <dennis.stene01@gmail.com>
2026-07-14 09:47:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Clean-Clothing-Brand-Project
---
+++ app.js
@@ -0,0 +1,20 @@
+const start = document.querySelector(".start");
+const preview = document.querySelector(".preview");
+const allClothing = document.querySelector(".all-clothing");
+const beginBtn = document.getElementById("clickMe");
+const seeMoreBtn = document.getElementById("seeMore");
+
+const images = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa0Hl104jY2t_YoneHMrQDTkNU2fo8S97UQ_u9srSxZA&s=10", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRccB8HoQGX-S828rBlHJ16MKFdq7zZ_44ymMOj0gAdVQ&s=10", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVk6MDo73GdDifSP_VcAxSeI1r9mgB5y-hay_tV4PnKQ&s=10"
+]
+
+beginBtn.addEventListener("click", () => {
+    start.style.display = "none";
+    preview.style.display = "block";
+});
+
+
+seeMoreBtn.addEventListener("click", () => {
+    preview.style.display = "none";
+    allClothing.style.display = "block";
+});
